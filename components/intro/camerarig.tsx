4604b6f03da87d5ff3fb93c5659d11a696601be5
Mahmoud Soliman <mahmoud00m99@gmail.com>
2026-07-30 22:07:37 +0300

    Add camera cinematic ,bloom and pulse animation

--- components/intro/camerarig.tsx
+++ components/intro/camerarig.tsx
@@ -1,5 +1,19 @@
 "use client";
 
-export default function CameraRig() {
-  return null;
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import * as THREE from "three";
+
+export default function cameraRig() {
+    const t = useRef(0);
+
+    useFrame(({ camera }) => {
+        t.current += 0.002;
+
+        camera.position.x = Math.sin(t.current) * 0.6;
+        camera.position.y = Math.cos(t.current * 0.7) * 0.3;
+        camera.position.z = 5 + Math.sin(t.current * 0.5) * 0.2;
+        camera.lookAt(new THREE.Vector3(0, 0, 0));
+    });
+    return null;
 }
--- components/intro/introscene.tsx
+++ components/intro/introscene.tsx
@@ -1,20 +1,34 @@
 "use client";
 
-import { Canvas } from "@react-three/fiber";
+import { Canvas  , useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import * as THREE from "three";
 import { Stars } from "@react-three/drei";
 import { PointLight } from "three";
+import { EffectComposer, Bloom } from "@react-three/postprocessing";
+import CameraRig from "./camerarig";
 
 function CoreLight() {
-  return (
-    <mesh>
-      <sphereGeometry args={[0.4, 64, 64]} />
-      <meshStandardMaterial
-      color="#3b82f6"
-      emissive="#3b82f6"
-      emissiveIntensity={5}
-      />
-    </mesh>
-  );
+    const meshRef = useRef<THREE.Mesh>(null);
+
+    useFrame(({ clock }) => {
+      if (!meshRef.current) return;
+
+      const t = clock.getElapsedTime();
+
+      const scale = 1 + Math.sin(t * 2) * 0.12;
+      meshRef.current.scale.set(scale, scale, scale);
+    });
+
+    return (
+        <mesh ref={meshRef}>
+            <sphereGeometry args={[0.4, 64, 64]} />
+            <meshStandardMaterial 
+            color="#3b82f6"
+            emissive="#3b82f6"
+            emissiveIntensity={5}/>
+        </mesh>
+    );
 }
 
 export default function IntroScene() {
@@ -43,7 +57,17 @@ export default function IntroScene() {
         fade
       />
 
-        <CoreLight />      
+        <CoreLight />    
+
+        <CameraRig />
+
+        <EffectComposer>
+            <Bloom
+            intensity={2}
+            luminanceThreshold={0}
+            luminanceSmoothing={0.9}
+            />
+        </EffectComposer> 
     </Canvas>
   );
 }
